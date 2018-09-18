ad5871c6ad6998a8195abd98bbf6dbde79a6360c
BT_1N3_24@BKTECH.LOCAL <BT_1N3_24@BKTECH.LOCAL>
2018-09-18 09:16:40 -0400

    Initial commit

---
+++ src/JHoang/JHoangLib.java
@@ -0,0 +1,9 @@
+package JHoang;
+
+public class JHoangLib {
+
+    public static void println(String str)
+    {
+        System.out.println(str);
+    }
+}
---
+++ src/JHoang/TestFile.java
@@ -0,0 +1,11 @@
+package JHoang;
+
+import static JHoang.JHoangLib.println;
+
+public class TestFile {
+
+    public static void main(String[] args)
+    {
+        println("I love a cup of Java in the morning.");
+    }
+}
